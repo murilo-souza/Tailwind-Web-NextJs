09110b53d3fe3916ebdf3b03e1d621c9fec6af61
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-02-19 19:24:06 -0300

    chore: tailwind config with prettier and eslint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,17 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/**/*.tsx",
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
+      maxWidth: {
+        app: '700px',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
