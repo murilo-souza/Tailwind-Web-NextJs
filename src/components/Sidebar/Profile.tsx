82170dc4ef0690d70122bf1e42fdd33c6a6bfe82
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-02-20 14:17:20 -0300

    feat: add profile component

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,29 @@
+import { LogOut } from 'lucide-react'
+
+export function Profile() {
+  return (
+    <div className="grid-cols-profile grid items-center gap-3">
+      <img
+        src="https://github.com/murilo-souza.png"
+        alt="Murilo"
+        className="h-10 w-10 rounded-full"
+      />
+
+      <div className="flex flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">
+          Murilo Souza
+        </span>
+        <span className="truncate text-sm text-zinc-500">
+          murilo.l.souza232@gmail.com
+        </span>
+      </div>
+
+      <button
+        type="button"
+        className="ml-auto rounded-md p-2 hover:bg-zinc-100"
+      >
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  )
+}
