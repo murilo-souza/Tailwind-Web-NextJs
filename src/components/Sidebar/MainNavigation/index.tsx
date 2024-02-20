245d9db0cbc2d0950f53005678ead03924dc49e1
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-02-19 21:02:09 -0300

    feat: add nav item component

---
+++ src/components/Sidebar/MainNavigation/index.tsx
@@ -0,0 +1,22 @@
+import {
+  BarChart,
+  CheckSquare,
+  Flag,
+  Home,
+  SquareStack,
+  User,
+} from 'lucide-react'
+import { NavItem } from './NavItem'
+
+export function MainNavigation() {
+  return (
+    <nav className="space-y-0.5">
+      <NavItem title="Home" icon={Home} />
+      <NavItem title="Dashboard" icon={BarChart} />
+      <NavItem title="Project" icon={SquareStack} />
+      <NavItem title="Tasks" icon={CheckSquare} />
+      <NavItem title="Reporting" icon={Flag} />
+      <NavItem title="Users" icon={User} />
+    </nav>
+  )
+}
