bbdd356ad6636dd8d1f079946d6c6fa2b2032033
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-02-22 19:47:35 -0300

    feat: add responsive design and theme dark on sidebar

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -11,10 +11,10 @@ export function Profile() {
       />
 
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Murilo Souza
         </span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           murilo.l.souza232@gmail.com
         </span>
       </div>
